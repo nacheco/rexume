c300ace6f62a92f804c116b3d1056115164f7177
Yi Chen <chenyiapp@yahoo.com>
2024-08-31 19:07:48 -0700

    completed step by step resume-builder

--- app/resume-builder/page.tsx
+++ app/resume-builder/page.tsx
@@ -4,29 +4,102 @@ import { useState, useEffect } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import StepForm from "@/components/StepForm";
 
-
 const ResumeBuilder = () => {
   const [step, setStep] = useState(0);
-  const [formData, setFormData] = useState({
+  const [aboutMe, setAboutMe] = useState({
     name: "",
     email: "",
     password: "",
   });
+  const [skills, setSkills] = useState([
+    {
+      description: "",
+    },
+  ]);
+  const [workExperiences, setWorkExperiences] = useState([
+    { employer: "", jobTitle: "", startDate: "", endDate: "", description: "" },
+  ]);
+  const [educationHistory, setEducationHistory] = useState([
+    {
+      school: "",
+      degree: "",
+      fieldOfStudy: "",
+      startDate: "",
+      endDate: "",
+      description: "",
+    },
+  ]);
 
   // Load data from localStorage
   useEffect(() => {
     const storedData = localStorage.getItem("resumeData");
     if (storedData) {
-      setFormData(JSON.parse(storedData));
+      const parsedData = JSON.parse(storedData);
+      setAboutMe(parsedData.aboutMe || {});
+      setWorkExperiences(parsedData.workExperiences || []);
+      setEducationHistory(parsedData.educationHistory || []);
+      setSkills(parsedData.skills || []);
     }
   }, []);
 
-  const handleChange = (key, value) => {
-    const updatedData = { ...formData, [key]: value };
-    setFormData(updatedData);
+  useEffect(() => {
+    localStorage.setItem(
+      "resumeData",
+      JSON.stringify({ aboutMe, workExperiences, educationHistory, skills })
+    );
+  }, [aboutMe, workExperiences, educationHistory, skills]);
 
-    // Save to localStorage
-    localStorage.setItem("resumeData", JSON.stringify(updatedData));
+  const handleAboutMeChange = (key, value) => {
+    setAboutMe((prev) => ({ ...prev, [key]: value }));
+  };
+
+  const handleWorkExperienceChange = (index, field, value) => {
+    const updatedExperiences = workExperiences.map((experience, i) =>
+      i === index ? { ...experience, [field]: value } : experience
+    );
+    setWorkExperiences(updatedExperiences);
+  };
+  const handleEducationHistoryChange = (index, field, value) => {
+    const updatedEducationHistory = educationHistory.map((edu, i) =>
+      i === index ? { ...edu, [field]: value } : edu
+    );
+    setEducationHistory(updatedEducationHistory);
+  };
+  const handleSkillChange = (index, field, value) => {
+    const updatedSkills = skills.map((skill, i) =>
+      i === index ? { ...skill, [field]: value } : skill
+    );
+    setSkills(updatedSkills);
+  };
+
+  // Add a new work experience
+  const addWorkExperience = () => {
+    setWorkExperiences([
+      ...workExperiences,
+      {
+        employer: "",
+        jobTitle: "",
+        startDate: "",
+        endDate: "",
+        description: "",
+      },
+    ]);
+  };
+  const addEducationHistory = () => {
+    setEducationHistory([
+      ...educationHistory,
+      {
+        school: "",
+        degree: "",
+        fieldOfStudy: "",
+        startDate: "",
+        endDate: "",
+        description: "",
+      },
+    ]);
+  };
+  const addSkill = () => {
+    setSkills([...skills, { description: "" }]);
   };
 
   const nextStep = () => setStep((prev) => prev + 1);
@@ -36,48 +109,118 @@ const ResumeBuilder = () => {
   const steps = [
     {
       step: 1,
-      fields: [{ name: "Name", type: "text", placeholder: "Enter your name" },
-        { name: "Title", type: "title", placeholder: "Enter your title: software engineer..." },
-        { name: "Phone Number", type: "phone", placeholder: "Enter your phone number" },
+      fields: [
+        { name: "Name", type: "text", placeholder: "Enter your name" },
+        {
+          name: "Title",
+          type: "title",
+          placeholder: "Enter your title: software engineer...",
+        },
+        {
+          name: "Phone Number",
+          type: "phone",
+          placeholder: "Enter your phone number",
+        },
         { name: "Email", type: "email", placeholder: "Enter your email" },
         {
-            name: "Location",
-            type: "location",
-            placeholder: "Enter the link to your location",
-          },
-          {
-            name: "LinkedIn",
-            type: "linkedin",
-            placeholder: "Enter the link to your LinkedIn page",
-          },
-          {
-            name: "Other Social Media Link",
-            type: "other1",
-            placeholder: "Enter more links such as github profile...",
-          },
-          {
-            name: "Other Social Media Link",
-            type: "other2",
-            placeholder: "Enter more links such as portfolio profile...",
-          },
+          name: "Location",
+          type: "location",
+          placeholder: "Enter the link to your location",
+        },
+        {
+          name: "LinkedIn",
+          type: "linkedin",
+          placeholder: "Enter the link to your LinkedIn page",
+        },
+        {
+          name: "Other Social Media Link",
+          type: "other1",
+          placeholder: "Enter more links such as github profile...",
+        },
+        {
+          name: "Other Social Media Link 2",
+          type: "other2",
+          placeholder: "Enter more links such as portfolio site...",
+        },
       ],
     },
     {
       step: 2,
-      fields: [{ name: "Employer", type: "text", placeholder: "Enter your Employer Name" },
-      { name: "Job Title", type: "text", placeholder: "Enter your Job Title" },
-      { name: "Start Date", type: "text", placeholder: "Enter the job start date" },
-      { name: "End Date", type: "text", placeholder: "Enter the job end date" },
-      { name: "Description", type: "text", placeholder: "Describe your work experience" },
-        
+      fields: [
+        {
+          name: "Employer",
+          type: "text",
+          placeholder: "Enter your Employer Name",
+        },
+        {
+          name: "Job Title",
+          type: "text",
+          placeholder: "Enter your Job Title",
+        },
+        {
+          name: "Start Date",
+          type: "text",
+          placeholder: "Enter the job start date",
+        },
+        {
+          name: "End Date",
+          type: "text",
+          placeholder: "Enter the job end date",
+        },
+        {
+          name: "Description",
+          type: "text",
+          placeholder: "Describe your work experience",
+        },
       ],
     },
-  
-
+    {
+      step: 3,
+      fields: [
+        {
+          name: "School",
+          type: "text",
+          placeholder: "Enter your School Name",
+        },
+        {
+          name: "Degree",
+          type: "text",
+          placeholder: "Enter your Degree",
+        },
+        {
+          name: "Field of Study",
+          type: "text",
+          placeholder: "Enter your field of study",
+        },
+        {
+          name: "Start Date",
+          type: "text",
+          placeholder: "Enter school start date",
+        },
+        {
+          name: "End Date",
+          type: "text",
+          placeholder: "Enter school end date",
+        },
+        {
+          name: "Description",
+          type: "text",
+          placeholder: "Describe your work experience",
+        },
+      ],
+    },
+    {
+      step: 4,
+      fields: [{
+        name: "Description",
+        type: "text",
+        placeholder: "Add a Skill",
+      },],
+    },
   ];
 
   return (
-    <div className="mx-auto my-50 w-[90vw] flex justify-center h-[80vh]" >
+    <div className="mx-auto my-50 w-[90vw] flex justify-center min-h-[fit-content]">
       <AnimatePresence mode="wait">
         {steps.map(
           (stepInfo, index) =>
@@ -86,8 +229,20 @@ const ResumeBuilder = () => {
                 key={`step${index}`}
                 step={stepInfo.step}
                 fields={stepInfo.fields}
-                formData={formData}
-                handleChange={handleChange}
+                aboutMe={aboutMe}
+                handleAboutMeChange={handleAboutMeChange}
+
+                workExperiences={workExperiences}
+                handleWorkExperienceChange={handleWorkExperienceChange}
+                addWorkExperience={addWorkExperience}
+
+                educationHistory={educationHistory}
+                handleEducationHistoryChange={handleEducationHistoryChange}
+                addEducationHistory={addEducationHistory}
+
+                skills={skills}
+                handleSkillChange={handleSkillChange}
+                addSkill={addSkill}
                 prevStep={index > 0 ? prevStep : null}
                 nextStep={index < steps.length - 1 ? nextStep : null}
               />
--- components/StepForm.tsx
+++ components/StepForm.tsx
@@ -1,40 +1,193 @@
-import { motion, AnimatePresence } from "framer-motion";
+"use client";
+
+import { motion } from "framer-motion";
 
 const StepForm = ({
   step,
   fields,
-  formData,
-  handleChange,
+
+  aboutMe,
+  handleAboutMeChange,
+
+  workExperiences,
+  addWorkExperience,
+  handleWorkExperienceChange,
+
+  educationHistory,
+  handleEducationHistoryChange,
+  addEducationHistory,
+
+  skills,
+  handleSkillChange,
+  addSkill,
+
   prevStep,
   nextStep,
 }) => {
-  console.log("fields", fields);
   return (
     <motion.div
-    className="flex flex-col items-center w-[100vw]"
+      className="flex flex-col items-center w-[100vw]"
       key={`step${step}`}
       initial={{ opacity: 0, x: 100 }}
       animate={{ opacity: 1, x: 0 }}
       exit={{ opacity: 0, x: -100 }}
     >
-      {fields.map((field) => (
-        <div key={field.name} className="w-[40vw] ">
-          <h2>{field.name}</h2>
-          <input
-          className="w-[100%]"
-            type={field.type}
-            placeholder={field.placeholder}
-            value={formData[field.name] || ""}
-            onChange={(e) => handleChange(field.name, e.target.value)}
-          />
-        </div>
-      ))}
-      <div>
-        {prevStep && <button onClick={prevStep}>Back</button>}
-        {nextStep ? (
-          <button onClick={nextStep}>Next</button>
-        ) : (
-          <button type="submit">Submit</button>
+      {/* Step 1: Basic Information Fields */}
+      {step === 1 && <h1 className="text-2xl">About You</h1>}
+      {step === 1 &&
+        fields.length &&
+        fields.map((field) => (
+          <div key={field?.name} className="w-[40vw] ">
+            <h2 >{field?.name}</h2>
+            <input
+              className="w-[100%] mb-4 mt-1"
+              type={field?.type}
+              placeholder={field.placeholder}
+              value={aboutMe[field?.name] || ""}
+              onChange={(e) => handleAboutMeChange(field.name, e.target.value)}
+            />
+          </div>
+        ))}
+
+      {/* Step 2: Work Experience Fields */}
+      {step === 2 && <h1 className="text-2xl">Work Experience</h1>}
+      {step === 2 &&
+        workExperiences.length &&
+        workExperiences.map((experience, index) => (
+          <div key={index} className="w-[40vw]  p-4 my-4">
+            <h2>Work Experience {index + 1}</h2>
+            {fields.map((field) => (
+              <div key={field.name} className="my-2">
+                <input
+                  className="w-[100%]"
+                  type={field.type}
+                  placeholder={field.placeholder}
+                  value={
+                    experience[field.name.toLowerCase().replace(" ", "")] || ""
+                  }
+                  onChange={(e) =>
+                    handleWorkExperienceChange(
+                      index,
+                      field.name.toLowerCase().replace(" ", ""),
+                      e.target.value
+                    )
+                  }
+                />
+              </div>
+            ))}
+          </div>
+        ))}
+
+      {/* Button to Add More Work Experience Sections */}
+      {step === 2 && (
+        <button
+          onClick={addWorkExperience}
+          className="my-4 bg-black text-white px-4 py-2 rounded"
+        >
+          Add Work Experience
+        </button>
+      )}
+
+      {/* Step 3: Education History Fields */}
+      {step === 3 && <h1 className="text-2xl">Education</h1>}
+      {step === 3 &&
+        educationHistory.map((edu, index) => (
+          <div key={index} className="w-[40vw]  p-4 my-4">
+            <h2>Education {index + 1}</h2>
+            {fields.map((field) => (
+              <div key={field.name} className="my-2">
+                <input
+                  className="w-[100%]"
+                  type={field.type}
+                  placeholder={field.placeholder}
+                  value={edu[field.name.toLowerCase().replace(" ", "")] || ""}
+                  onChange={(e) =>
+                    handleEducationHistoryChange(
+                      index,
+                      field.name.toLowerCase().replace(" ", ""),
+                      e.target.value
+                    )
+                  }
+                />
+              </div>
+            ))}
+          </div>
+        ))}
+
+      {/* Button to Add More Education History Sections */}
+      {step === 3 && (
+        <button
+          onClick={addEducationHistory}
+          className="my-4 bg-black text-white px-4 py-2 rounded"
+        >
+          Add Education
+        </button>
+      )}
+      {/* Step 4: Skill Fields */}
+      {step === 4 && <h1 className="text-2xl">Skills</h1>}
+      {step === 4 &&
+        skills.length &&
+        skills.map((skill, index) => (
+          <div key={index} className="w-[40vw] p-4 my-4">
+            <h2 >Skill {index + 1}</h2>
+            {fields.length &&
+              fields?.map((field) => (
+                <div key={field.name} className="my-2">
+                  <input
+                    className="w-[100%]"
+                    type={field.type}
+                    placeholder={field.placeholder}
+                    value={
+                      skill[field?.name?.toLowerCase().replace(" ", "")] || ""
+                    }
+                    onChange={(e) =>
+                      handleSkillChange(
+                        index,
+                        field.name.toLowerCase().replace(" ", ""),
+                        e.target.value
+                      )
+                    }
+                  />
+                </div>
+              ))}
+          </div>
+        ))}
+
+      {/* Button to Add More Education History Sections */}
+      {step === 4 && (
+        <button
+          onClick={addSkill}
+          className="my-4 bg-black text-white px-4 py-2 rounded"
+        >
+          Add Skill
+        </button>
+      )}
+
+      {/* Navigation Buttons */}
+      <div className="flex space-x-4 my-8">
+        {prevStep && (
+          <button
+            onClick={prevStep}
+            className="bg-gray-500 text-white px-4 py-2 rounded"
+          >
+            Previous
+          </button>
+        )}
+        {nextStep && (
+          <button
+            onClick={nextStep}
+            className="bg-[#FB8C3E] text-white px-4 py-2 rounded"
+          >
+            Next
+          </button>
+        )}
+        {!nextStep && (
+          <button
+            type="submit"
+            className="bg-[#FB8C3E] text-white px-4 py-2 rounded"
+          >
+            Generate Resume
+          </button>
         )}
       </div>
     </motion.div>
